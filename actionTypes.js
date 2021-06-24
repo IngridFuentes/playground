ec1584ff2467c126896f164313fa699794e8678d
Ingrid Fuentes <49660565+IngridFuentes@users.noreply.github.com>
2021-06-24 19:46:40 -0400

    REDUX-THUNK WORKING

---
+++ actionTypes.js
@@ -0,0 +1,5 @@
+export const SLIDE_RIGHT = "SLIDE_RIGHT";
+export const SLIDE_LEFT = "SLIDE_LEFT";
+export const UPDATE_DATA = "UPDATE_DATA"
+           
+// export const STORE_RECORDS = "STORE_RECORDS";
